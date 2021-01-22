669dcd6b700a97827ec0c6040bef7a02bfd68132
zyxnine9 <jianghurenzhe@qq.com>
2021-01-22 17:36:51 +0800

    提交版本代码

--- src/views/Dashboard/components/Personal.vue
+++ src/views/Dashboard/components/Personal.vue
@@ -4,7 +4,11 @@
       <h1>学术外联试用申请</h1>
       <el-form label-position="left" label-width="100px">
         <el-form-item label="申请人">
-          <el-input v-model="user.username" :disabled="!submitable"></el-input>
+          <el-input
+            v-model="user.username"
+            placeholder="请上传身份证自动识别"
+            :disabled="true"
+          ></el-input>
         </el-form-item>
         <el-form-item label="所在单位">
           <el-input
@@ -25,7 +29,11 @@
           <el-input v-model="user.mobile" :disabled="true"></el-input>
         </el-form-item>
         <el-form-item label="身份证号码">
-          <el-input v-model="user.idCard" :disabled="!submitable"></el-input>
+          <el-input
+            v-model="user.idCard"
+            placeholder="请上传身份证自动识别"
+            :disabled="true"
+          ></el-input>
         </el-form-item>
         <el-form-item label="申请理由">
           <el-input
@@ -52,13 +60,15 @@
               ref="front"
               title="身份证正面"
               alt="身份证正面"
-              @click="$refs.uploadFront.click()"
+              src="@/assets/front.svg"
+              @click="frontClick"
             />
             <img
               ref="back"
               title="身份证背面"
               alt="身份证背面"
-              @click="$refs.uploadBack.click()"
+              @click="backClick"
+              src="@/assets/back.svg"
             />
           </div>
         </div>
@@ -151,43 +161,54 @@ export default {
   },
   methods: {
     onFileSelectedFront(event) {
-      if (event.target.files[0]) {
-        this.idCardFront = event.target.files[0];
+      if (!event.target.files[0]) {
+        console.log("没文件")
+        return;
       }
-      const reader = new FileReader();
-      let format = this.idCardFront.name.split(".");
-
-      reader.onload = () => {
-        this.$refs.front.src = reader.result
-      };
-      reader.readAsDataURL(this.idCardFront);
-      format = format[format.length - 1];
+      let format = event.target.files[0].name.split(".").pop();
       if (format != "png" && format != "jpg" && format != "jpeg") {
-        this.idCardFront = null;
         Message({
           type: "warning",
           message: "请身份证正面图片",
         });
+        return;
       }
+      this.idCardFront = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.$refs.front.src = reader.result;
+      };
+      reader.readAsDataURL(this.idCardFront);
+      format = format[format.length - 1];
     },
     onFileSelectedBack(event) {
-      if (event.target.files[0]) {
-        this.idCardBack = event.target.files[0];
+      if (!event.target.files[0]) {
+        return;
       }
+      let format = event.target.files[0].name.split(".").pop();
+      if (format != "png" && format != "jpg" && format != "jpeg") {
+        Message({
+          type: "warning",
+          message: "请身份证背面图片",
+        });
+        return;
+      }
+      this.idCardBack = event.target.files[0];
       const reader = new FileReader();
-      let format = this.idCardBack.name.split(".");
-
       reader.onload = () => {
         this.$refs.back.src = reader.result;
       };
       reader.readAsDataURL(this.idCardBack);
       format = format[format.length - 1];
-      if (format != "png" && format != "jpg" && format != "jpeg") {
-        this.idCardBack = null;
-        Message({
-          type: "warning",
-          message: "请身份证背面图片",
-        });
+    },
+    backClick() {
+      if (this.submitable) {
+        this.$refs.uploadBack.click();
+      }
+    },
+    frontClick() {
+      if (this.submitable) {
+        this.$refs.uploadFront.click();
       }
     },
     getUserInfo() {
@@ -197,15 +218,13 @@ export default {
       })
         .then((res) => {
           this.user = res.data;
-          console.log(res.data)
           this.$cookies.set("user", res.data, "1h");
           this.$emit("setTitle");
           if (res.data.idCard) {
             this.submitable = false;
-            this.$refs.front.src = res.data.idCardImg_1
-            this.$refs.back.src = res.data.idCardImg_2
+            this.$refs.front.src = res.data.idCardImg_1;
+            this.$refs.back.src = res.data.idCardImg_2;
           }
-
         })
         .catch((error) => {
           console.log(error);
@@ -238,16 +257,28 @@ export default {
         alert("请勾选所有的承诺");
         return;
       }
-      if (!this.user.idCard) {
-        alert("请填写身份证号码");
-        return;
-      }
       if (!this.idCardFront) {
         alert("请提交身份证正面图片");
         return;
       }
       if (!this.idCardBack) {
         alert("请提交身份证背面图片");
+        return;
+      }
+      if (!this.user.note) {
+        alert("请提交申请理由");
+        return;
+      }
+      if (!this.user.organization_topLevel) {
+        alert("请提交所在单位");
+        return;
+      }
+      if (!this.user.organization_secondLevel) {
+        alert("请提交所在部门");
+        return;
+      }
+      if (!this.user.career) {
+        alert("请提交职务或岗位");
         return;
       }
       if (this.submitable) {
@@ -260,7 +291,26 @@ export default {
             this.getUserInfo();
           })
           .catch((error) => {
-            console.log(error);
+            if (error.response) {
+              if (error.response.status) {
+                if (error.response.status == 406) {
+                  Message({
+                    message: "请重新上传身份证照片",
+                    type: "warning",
+                  });
+                } else {
+                  Message({
+                    message: "服务器出错",
+                    type: "warning",
+                  });
+                }
+              }
+            } else {
+              Message({
+                message: "未知的错误发生了",
+                type: "warning",
+              });
+            }
           });
       } else {
         alert("不能重复提交");
@@ -310,6 +360,8 @@ export default {
           "不利用学术外联 APP 制作、复制、查阅和传播法律、行政法规禁止的内容。",
           "自觉健康上网、文明上网。",
         ];
+      } else {
+        this.type = [];
       }
     },
   },
@@ -348,6 +400,7 @@ export default {
     height: 200px;
     width: 350px;
     border-radius: 8px;
+    border: dashed black 1px;
     &:hover {
       cursor: pointer;
     }
