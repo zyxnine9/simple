8684f699a5a727d2f186d7db60833a1813031113
zyxnine9 <jianghurenzhe@qq.com>
2021-01-28 10:06:31 +0800

    增加验证

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,19 +1,17 @@
 import axios from 'axios'
+// import { Message } from 'element-ui';
 import { getToken } from "./auth"
 
 const instance = axios.create({
-    baseURL: "http://192.168.203.66:20002",
-    timeout: 20000,
+    timeout: 1000 * 10,
 })
 
 
 // 拦截 请求
 instance.interceptors.request.use(config => {
     const isToken = (config.headers || {}).isToken === false;
-   if(process.env.NODE_ENV === "production"){
-        const pref = config.url.split("/")[1]
-        if (pref === "api")  config.baseURL = "http://health.pcl.ac.cn/"
-        else if (pref === "get_token") config.baseURL = "http://health.pcl.ac.cn/"
+    if (process.env.NODE_ENV === "production") {
+        config.baseURL = "https://acad-access.pcl.ac.cn/"
     }
     if (getToken() && !isToken) {
         config.headers['Authorization'] = 'Token ' + getToken()
@@ -30,8 +28,7 @@ instance.interceptors.response.use(res => {
     return res;
 }, error => {
     console.log('响应错误')
-    console.log(error.response)
-    // alert(error.response.data)
+  
     return Promise.reject(error);
 })
 
--- src/views/Dashboard/components/Personal.vue
+++ src/views/Dashboard/components/Personal.vue
@@ -1,29 +1,44 @@
 <template>
   <div class="dashboard-container">
-    <div class="form">
+    <div v-loading="onSubmit" class="form">
       <h1>学术外联试用申请</h1>
+      <div class="p-hint">
+        <p>
+          1、系统从申请人上传的身份证照片中提取姓名、身份证号信息，不需要申请人填写。
+        </p>
+        <p>
+          2、填写的手机号码必须是申请人经过电信运营商实名认证，该号码将用于登陆“学术外联”的身份认证。
+        </p>
+      </div>
       <el-form label-position="left" label-width="100px">
         <el-form-item label="申请人">
           <el-input
             v-model="user.username"
+            maxlength="20"
             placeholder="请上传身份证自动识别"
             :disabled="true"
           ></el-input>
         </el-form-item>
         <el-form-item label="所在单位">
           <el-input
+            maxlength="20"
             v-model="user.organization_topLevel"
             :disabled="!submitable"
           ></el-input>
         </el-form-item>
         <el-form-item label="所在部门">
           <el-input
+            maxlength="20"
             v-model="user.organization_secondLevel"
             :disabled="!submitable"
           ></el-input>
         </el-form-item>
         <el-form-item label="职务或岗位">
-          <el-input v-model="user.career" :disabled="!submitable"></el-input>
+          <el-input
+            v-model="user.career"
+            maxlength="20"
+            :disabled="!submitable"
+          ></el-input>
         </el-form-item>
         <el-form-item label="手机号码">
           <el-input v-model="user.mobile" :disabled="true"></el-input>
@@ -40,6 +55,7 @@
             type="textarea"
             v-model="user.note"
             :disabled="!submitable"
+            maxlength="200"
           ></el-input>
         </el-form-item>
         <div class="id-card">
@@ -72,64 +88,87 @@
             />
           </div>
         </div>
-        <el-form-item v-if="submitable" label="本人承诺" prop="type">
-          <el-switch v-model="all" active-text="完成承诺"> </el-switch>
-          <el-checkbox-group style="text-align: left" v-model="type">
+        <el-form-item label="本人承诺" prop="type">
+          <el-switch v-model="all" v-if="submitable" active-text="完成所有承诺">
+          </el-switch>
+          <el-checkbox-group
+            v-if="submitable"
+            style="text-align: left"
+            v-model="type"
+          >
             <el-checkbox
               label="不对外宣传和公开学术外联及 APP 相关事宜。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="不利用学术外联 APP 进行危害国家安全、泄露国家秘密。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="不利用学术外联 APP 进行颠覆国家政权、破坏国家统一。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="不利用学术外联 APP 进行损害国家、社会、集体和第三方的荣誉和利益。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="不利用学术外联 APP 进行煽动民族仇恨、民族歧视，破坏民族团结。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="不利用学术外联 APP 进行侮辱或者诽谤他人，侵害他人合法权益。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="不利用学术外联 APP 进行散布谣言，扰乱社会秩序，破坏社会稳定。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="不利用学术外联 APP 进行破坏国家宗教政策，宣扬邪教和封建迷信。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="不利用学术外联 APP 制作、复制、查阅和传播法律、行政法规禁止的内容。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
             <el-checkbox
               label="自觉健康上网、文明上网。"
               name="type"
+              :disabled="!submitable"
             ></el-checkbox>
           </el-checkbox-group>
+          <div class="p-commit" v-else>
+            <p v-for="(e, i) in type" :key="i">{{ e }}</p>
+          </div>
         </el-form-item>
-
-        <el-button v-if="submitable" @click="submit" type="primary"
-          >确定</el-button
-        >
-        <el-alert
-          v-else
-          :closable="false"
-          :title="`申请已经于 ${time} 提交。无法重复提交`"
-          type="success"
-          effect="dark"
-        >
-        </el-alert>
       </el-form>
+    </div>
+    <div style="text-align: center">
+      <el-button
+        v-if="submitable"
+        :loading="onSubmit"
+        @click="submit"
+        type="primary"
+        >确定</el-button
+      >
+      <el-alert
+        v-else
+        :closable="false"
+        :title="`申请已经于 ${time} 提交。无法重复提交`"
+        type="success"
+        effect="dark"
+      >
+      </el-alert>
     </div>
   </div>
 </template>
@@ -144,7 +183,7 @@ export default {
       all: false,
       submitable: true,
       time: "",
-
+      onSubmit: false,
       idCardFront: null,
       idCardBack: null,
       user: {
@@ -161,10 +200,14 @@ export default {
   },
   methods: {
     onFileSelectedFront(event) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.$refs.front.src = reader.result;
+      };
       if (!event.target.files[0]) {
-        console.log("没文件")
         return;
       }
+
       let format = event.target.files[0].name.split(".").pop();
       if (format != "png" && format != "jpg" && format != "jpeg") {
         Message({
@@ -173,16 +216,43 @@ export default {
         });
         return;
       }
+      if (event.target.files[0].size > 1024 * 1024) {
+        Message({
+          type: "warning",
+          message: "图片大小不得超过1M",
+        });
+        return;
+      }
       this.idCardFront = event.target.files[0];
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.$refs.front.src = reader.result;
-      };
+
       reader.readAsDataURL(this.idCardFront);
-      format = format[format.length - 1];
     },
     onFileSelectedBack(event) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.$refs.back.src = reader.result;
+      };
       if (!event.target.files[0]) {
+        return;
+      }
+      if (this.type.length < 10) {
+        alert("请勾选所有的承诺");
+        return;
+      }
+      if (!this.user.note) {
+        alert("请填写申请理由");
+        return;
+      }
+      if (!this.user.organization_topLevel) {
+        alert("请填写所在单位");
+        return;
+      }
+      if (!this.user.organization_secondLevel) {
+        alert("请填写所在部门");
+        return;
+      }
+      if (!this.user.career) {
+        alert("请填写职务或岗位");
         return;
       }
       let format = event.target.files[0].name.split(".").pop();
@@ -193,20 +263,63 @@ export default {
         });
         return;
       }
+      if (event.target.files[0].size > 1024 * 1024) {
+        Message({
+          type: "warning",
+          message: "图片大小不得超过1M",
+        });
+        return;
+      }
       this.idCardBack = event.target.files[0];
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.$refs.back.src = reader.result;
-      };
+
       reader.readAsDataURL(this.idCardBack);
-      format = format[format.length - 1];
     },
     backClick() {
+      if (this.type.length < 10) {
+        alert("请勾选所有的承诺");
+        return;
+      }
+      if (!this.user.note) {
+        alert("请填写申请理由");
+        return;
+      }
+      if (!this.user.organization_topLevel) {
+        alert("请填写所在单位");
+        return;
+      }
+      if (!this.user.organization_secondLevel) {
+        alert("请填写所在部门");
+        return;
+      }
+      if (!this.user.career) {
+        alert("请填写职务或岗位");
+        return;
+      }
       if (this.submitable) {
         this.$refs.uploadBack.click();
       }
     },
     frontClick() {
+      if (this.type.length < 10) {
+        alert("请勾选所有的承诺");
+        return;
+      }
+      if (!this.user.note) {
+        alert("请填写申请理由");
+        return;
+      }
+      if (!this.user.organization_topLevel) {
+        alert("请填写所在单位");
+        return;
+      }
+      if (!this.user.organization_secondLevel) {
+        alert("请填写所在部门");
+        return;
+      }
+      if (!this.user.career) {
+        alert("请填写职务或岗位");
+        return;
+      }
       if (this.submitable) {
         this.$refs.uploadFront.click();
       }
@@ -226,21 +339,9 @@ export default {
             this.$refs.back.src = res.data.idCardImg_2;
           }
         })
-        .catch((error) => {
-          console.log(error);
-        });
+        .catch(() => {});
     },
     async submit() {
-      // const data = {
-      //   workOrderType: "1",
-      //   idCard: this.user.idCard,
-      //   username: this.user.username,
-      //   career: this.user.career,
-      //   organization_name: this.user.organization_topLevel,
-      //   organization_topLevel: this.user.organization_topLevel,
-      //   organization_secondLevel: this.user.organization_secondLevel,
-      //   note: this.user.note,
-      // };
       const fd = new FormData();
       fd.append("workOrderType", 1);
       fd.append("idCard", this.user.idCard);
@@ -257,40 +358,44 @@ export default {
         alert("请勾选所有的承诺");
         return;
       }
-      if (!this.idCardFront) {
-        alert("请提交身份证正面图片");
-        return;
-      }
-      if (!this.idCardBack) {
-        alert("请提交身份证背面图片");
-        return;
-      }
       if (!this.user.note) {
-        alert("请提交申请理由");
+        alert("请填写申请理由");
         return;
       }
       if (!this.user.organization_topLevel) {
-        alert("请提交所在单位");
+        alert("请填写所在单位");
         return;
       }
       if (!this.user.organization_secondLevel) {
-        alert("请提交所在部门");
+        alert("请填写所在部门");
         return;
       }
       if (!this.user.career) {
-        alert("请提交职务或岗位");
+        alert("请填写职务或岗位");
         return;
       }
+      if (!this.idCardFront) {
+        alert("请填写身份证正面图片");
+        return;
+      }
+      if (!this.idCardBack) {
+        alert("请填写身份证背面图片");
+        return;
+      }
+
       if (this.submitable) {
+        this.onSubmit = true;
         await request({
           url: "/api/CreateWorkOrder/",
           method: "post",
           data: fd,
         })
           .then(() => {
+            this.onSubmit = false;
             this.getUserInfo();
           })
           .catch((error) => {
+            this.onSubmit = false;
             if (error.response) {
               if (error.response.status) {
                 if (error.response.status == 406) {
@@ -305,6 +410,11 @@ export default {
                   });
                 }
               }
+            } else if (error.message.includes("timeout")) {
+              Message({
+                message: "网络超时",
+                type: "warning",
+              });
             } else {
               Message({
                 message: "未知的错误发生了",
@@ -344,7 +454,21 @@ export default {
   },
   watch: {
     submitable: function () {
-      if (!this.submitable) this.getCreateTime();
+      if (!this.submitable) {
+        this.type = [
+          "不对外宣传和公开学术外联及 APP 相关事宜。",
+          "不利用学术外联 APP 进行危害国家安全、泄露国家秘密。",
+          "不利用学术外联 APP 进行颠覆国家政权、破坏国家统一。",
+          "不利用学术外联 APP 进行损害国家、社会、集体和第三方的荣誉和利益。",
+          "不利用学术外联 APP 进行煽动民族仇恨、民族歧视，破坏民族团结。",
+          "不利用学术外联 APP 进行侮辱或者诽谤他人，侵害他人合法权益。",
+          "不利用学术外联 APP 进行散布谣言，扰乱社会秩序，破坏社会稳定。",
+          "不利用学术外联 APP 进行破坏国家宗教政策，宣扬邪教和封建迷信。",
+          "不利用学术外联 APP 制作、复制、查阅和传播法律、行政法规禁止的内容。",
+          "自觉健康上网、文明上网。",
+        ];
+        this.getCreateTime();
+      }
     },
     all: function () {
       if (this.all) {
@@ -404,6 +528,22 @@ export default {
     &:hover {
       cursor: pointer;
     }
+  }
+}
+.p-hint {
+  p {
+    margin: 12px;
+  }
+  font-size: 0.8em;
+  color: gray;
+}
+.p-commit {
+  text-align: left;
+  p {
+    color: gray;
+    font-weight: bold;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+      "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   }
 }
 </style>
